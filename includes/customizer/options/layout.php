8cef3e77a87c47de5574588aacc5802a3134123f
khkesav <mindless3wizards@gmail.com>
2023-09-07 10:51:41 +0545

    Update layout

--- includes/customizer/options/layout.php
+++ includes/customizer/options/layout.php
@@ -1,55 +1,49 @@
 <?php
 /*--------------------------------------------
-	Layout Panel
+## Layout Panel
 ---------------------------------------------*/
-$wp_customize->add_panel( 'knote_layout_panel',
+$wp_customize->add_section( 'knote_layout_section',
     array(
         'title'          => esc_html__( 'Layout', 'knote' ),
         'capability'     => 'edit_theme_options',
-        'priority'       => 20,
+        'priority'       => 16,
     )
 );
 
-/*--------------------------------------------
-	Page Section
----------------------------------------------*/
-$wp_customize->add_setting(
-	'knote_enable_page_header',
+$wp_customize->add_setting( 'knote_site_layout',
 	array(
-		'default'           => 0,
-		'sanitize_callback' => 'knote_sanitize_checkbox',
+		'default'           => 'default',
+		'sanitize_callback' => 'sanitize_key',
 	)
 );
+
 $wp_customize->add_control(
-	new Knote_Control_Switch(
+	new Knote_Control_RadioImage(
 		$wp_customize,
-		'knote_enable_page_header',
+		'knote_site_layout',
 		array(
-			'label'         	=> esc_html__( 'Enable page heading', 'knote' ),
-			'section'       	=> 'knote_page_section',
-			'settings'      	=> 'knote_enable_page_header',
+			'label'    => esc_html__( 'Site layout', 'knote' ),
+			'section'  => 'knote_layout_section',
+			'columns'  => 'one-half',
+			'choices'  => array(
+				'default' => array(
+					'label' => esc_html__( 'Default', 'knote' ),
+					'url'   => '%s/assets/admin/src/layout/post-wide.svg'
+                ),
+				'boxed' => array(
+					'label' => esc_html__( 'Boxed', 'knote' ),
+					'url'   => '%s/assets/admin/src/layout/post-fullwidth.svg'
+				),
+				'fixed' => array(
+					'label' => esc_html__( 'Fixed', 'knote' ),
+					'url'   => '%s/assets/admin/src/layout/post-fullwidth.svg'
+				)
+			),
+			'priority'  => 10
 		)
 	)
 );
 
-$wp_customize->add_setting(
-	'knote_enable_page_breadcrumb',
-	array(
-		'default'           => 0,
-		'sanitize_callback' => 'knote_sanitize_checkbox',
-	)
-);
-$wp_customize->add_control(
-	new Knote_Control_Switch(
-		$wp_customize,
-		'knote_enable_page_breadcrumb',
-		array(
-			'label'         	=> esc_html__( 'Enable page breadcrumb', 'knote' ),
-			'section'       	=> 'knote_page_section',
-			'settings'      	=> 'knote_enable_page_breadcrumb',
-		)
-	)
-);
 
 
 
