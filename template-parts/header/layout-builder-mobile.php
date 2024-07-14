a6f971f01e3fb5ae074c447426dfeee7606acf46
khkesav <mindless3wizards@gmail.com>
2024-07-14 06:41:42 +0545

    Update version

--- template-parts/header/layout-builder-mobile.php
+++ template-parts/header/layout-builder-mobile.php
@@ -54,7 +54,7 @@ if ( !empty( $elements ) ){ ?>
         <div class="block-header--drawer">
             <button type="button" class="menu-toggle-close medium-down--show js-drawer-close js-close" aria-controls="drawer-navigation-menu" aria-expanded="false" data-drawer-toggle>
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="icon icon-close"><line x1="18" y1="6" x2="6" y2="18"/><line x1="6" y1="6" x2="18" y2="18"/></svg>
-                <small class="screen-reader-text"><?php esc_html_e( 'Menu', 'knote' ); ?></small>
+                <small class="screen-reader-text"><?php esc_html_e( 'Menu close', 'knote' ); ?></small>
             </button>
         </div>
         <div class="navigation-content">
